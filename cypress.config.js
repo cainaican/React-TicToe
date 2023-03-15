5d5e2475d5e4c12b26a72a918bfbaae943a7c7c1
Musin Dinislam <MusinDi@inmeta.ru>
2023-03-15 10:31:59 +0500

    added config and yml

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  
+  projectId: "pnetiu",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
